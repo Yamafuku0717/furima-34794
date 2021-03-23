d988359e28c0aaebc03d14307f46ed5fc5ebb60f
Yamafuku0717 <toyaman8339@yahoo.co.jp>
2021-03-23 12:06:19 +0900

    itemモデルの単体テストコード実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_prams)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -15,8 +15,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  #private
-  #def item_params
-    #params.require(:item).permit(:name, :description, :price, :category_id, :sales_status_id, :shipping_free_status_id, :prefecture_id, :scheduled_delivery_id, :image)merge(user_id: current_user.id)
-  #end
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :category_id, :sales_status_id, :shipping_free_status_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :salesstatus
-  belongs_to :shippingfreestatus
+  belongs_to :sales_status
+  belongs_to :shipping_free_status
   belongs_to :prefecture
-  belongs_to :scheduleddelivery
+  belongs_to :scheduled_delivery
   
   belongs_to :user
   has_one_attached :image
   
-  
-  
-
   with_options presence: true do
+    validates :image
     validates :name
     validates :description
-    validates :price, format: { with: /\A[0-9]+\z/}, inclusion: {in: 300..9999999}
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :category_id
     validates :sales_status_id
     validates :shipping_free_status_id
